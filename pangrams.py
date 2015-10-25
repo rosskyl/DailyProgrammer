bba2cf3df91488a937c62f45c662bd1830e8e935
rosskyl <rosskyl@mnstate.edu>
2015-10-25 17:48:59 -0500

    Added multiple pangrams to pangrams.py

--- pangrams.py
+++ pangrams.py
@@ -3,24 +3,23 @@ https://www.reddit.com/r/dailyprogrammer/comments/1pwl73/11413_challenge_139_eas
 """
 from string import ascii_lowercase as AtoZ
 
-#numLines = int(input("Enter the number of lines: "))
+numLines = int(input("Enter the number of lines: "))
 
-#for i in range(numLines):
-##TODO need to make it do it for more than one line
+for i in range(numLines):
+    line = input("Enter the sentence: ")
+    charDict = {}
+    for char in AtoZ:
+        charDict[char] = 0
 
-line = input("Enter the sentence: ")
-charDict = {}
-for char in AtoZ:
-    charDict[char] = 0
+    line = line.lower()
+    for char in line:
+        if char in AtoZ:
+            charDict[char] += 1
 
-line = line.lower()
-for char in line:
-    if char in AtoZ:
-        charDict[char] += 1
+    isPangram = True
+    for value in charDict.values():
+        if not value > 0:
+            isPangram = False
 
-isPangram = True
-for value in charDict.values():
-    if not value > 0:
-        isPangram = False
-
-print(isPangram)
+    print(isPangram)
+    print(charDict)
