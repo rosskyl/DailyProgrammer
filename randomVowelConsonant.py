0303605e4f1c1abbeaa171879148d93f15abc165
rosskyl <rosskyl@mnstate.edu>
2015-10-27 17:09:11 -0500

    Changed randomVowelConsonant.py to take multiple inputs

--- randomVowelConsonant.py
+++ randomVowelConsonant.py
@@ -5,19 +5,23 @@ from random import randrange
 
 vowels = "aeiouy"
 consonants = "bcdfghjklmnpqrstvwxz"
-word = ""
+
+print("press enter when done")
 line = input("Enter Cs and Vs: ")
-for char in line:
-    if char.lower() == "c":
-        letter = consonants[randrange(0,len(consonants))]
-        if char == "C":
-            letter = letter.upper()
-    elif char.lower() == "v":
-        letter = vowels[randrange(0,len(vowels))]
-        if char == "V":
-            letter = letter.upper()
-    else:
-        print("must only contain C,V,c,v")
-        exit(1)
-    word += letter
-print(word)
+while line != "":
+    word = ""
+    for char in line:
+        if char.lower() == "c":
+            letter = consonants[randrange(0,len(consonants))]
+            if char == "C":
+                letter = letter.upper()
+        elif char.lower() == "v":
+            letter = vowels[randrange(0,len(vowels))]
+            if char == "V":
+                letter = letter.upper()
+        else:
+            print("must only contain C,V,c,v")
+            exit(1)
+        word += letter
+    print(word)
+    line = input("Enter Cs and Vs: ")
