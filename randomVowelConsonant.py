92ed4aa2cedb1b65b3d279c543bfbd428e0d8218
rosskyl <rosskyl@mnstate.edu>
2015-10-27 17:06:32 -0500

    Changed randomVowelConsonant.py to handle a single input

--- randomVowelConsonant.py
+++ randomVowelConsonant.py
@@ -1,3 +1,23 @@
 """
 https://www.reddit.com/r/dailyprogrammer/comments/3q9vpn/20151026_challenge_238_easy_consonants_and_vowels/
 """
+from random import randrange
+
+vowels = "aeiouy"
+consonants = "bcdfghjklmnpqrstvwxz"
+word = ""
+line = input("Enter Cs and Vs: ")
+for char in line:
+    if char.lower() == "c":
+        letter = consonants[randrange(0,len(consonants))]
+        if char == "C":
+            letter = letter.upper()
+    elif char.lower() == "v":
+        letter = vowels[randrange(0,len(vowels))]
+        if char == "V":
+            letter = letter.upper()
+    else:
+        print("must only contain C,V,c,v")
+        exit(1)
+    word += letter
+print(word)
